48828cd79841e2d0d55d22c93aeff6b588de98ff
ihsan-null <ihsanulz92@gmail.com>
2021-09-27 23:41:29 +0700

    add html template

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, blueprints
+from flask import Blueprint, blueprints, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 
